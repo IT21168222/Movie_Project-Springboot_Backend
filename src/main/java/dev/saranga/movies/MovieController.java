3c093f69ddae7fc82ead5af33250d7c2e5850f26
Saranga Gunasekara <it21168222@my.sliit.lk>
2024-01-17 01:27:44 +0530

    Commit Version 1.0

---
+++ src/main/java/dev/saranga/movies/MovieController.java
@@ -0,0 +1,34 @@
+package dev.saranga.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/v1/movies/")
+@CrossOrigin(origins = "*")
+public class MovieController
+{
+    @Autowired
+    private MovieService movieService;
+    @GetMapping
+    public ResponseEntity<List<Movie>> getAllMovies(){
+
+        return new ResponseEntity<List<Movie>>(movieService.allMovies(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{imdbId}")
+    public ResponseEntity <Optional<Movie>> getSingleMovies(@PathVariable String imdbId){
+
+        return new ResponseEntity<Optional<Movie>>(movieService.singleMovie(imdbId), HttpStatus.OK);
+    }
+}
